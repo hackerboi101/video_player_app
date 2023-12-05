0ee2f9b0f7987520a99f774e6ace20fdb2d102fc
Asif Iqbal <asif.iq.024@gmail.com>
2023-12-05 06:51:43 +0600

    HomePage (a tiny bit of proper styling left to do)

---
+++ lib/controllers/video_controller.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:video_player_app/models/videos_model.dart';
+import 'package:intl/intl.dart';
+
+class VideoController extends GetxController {
+  RxList<Results> videosList = <Results>[].obs;
+  RxBool isLoading = false.obs;
+  RxInt page = 1.obs;
+
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  Future<void> onInit() async {
+    super.onInit();
+    await fetchVideos();
+    scrollController.addListener(() async {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        await fetchVideos();
+      }
+    });
+  }
+
+  Future<void> fetchVideos() async {
+    try {
+      if (!isLoading.value) {
+        isLoading(true);
+        final response = await http.get(Uri.parse(
+            'https://test-ximit.mahfil.net/api/trending-video/1?page=${page.value}'));
+        if (response.statusCode == 200) {
+          final Map<String, dynamic> data =
+              json.decode(utf8.decode(response.bodyBytes));
+          final Videos videosData = Videos.fromJson(data);
+
+          if (videosData.results != null) {
+            videosList.addAll(videosData.results!);
+            page++;
+          }
+        }
+      }
+    } catch (error) {
+      debugPrint('Error fetching videos: $error');
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  String formattedDate(String? date) {
+    if (date == null) {
+      return '';
+    }
+
+    final DateTime parsedDate = DateTime.parse(date);
+    final DateFormat formatter = DateFormat.yMMMd();
+    return formatter.format(parsedDate);
+  }
+}
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,16 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:video_player_app/controllers/video_controller.dart';
+import 'package:video_player_app/models/videos_model.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final VideoController videoController = Get.put(VideoController());
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
+        controller: videoController.scrollController,
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [],
+          children: [
+            const SizedBox(height: 30.0),
+            const Text(
+              'Trending Videos',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Obx(
+              () {
+                if (videoController.isLoading.value &&
+                    videoController.page == RxInt(1)) {
+                  return const Center(child: CircularProgressIndicator());
+                } else {
+                  return ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: videoController.videosList.length,
+                    itemBuilder: (context, index) {
+                      final Results video = videoController.videosList[index];
+
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            height: 200.0,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(video.thumbnail ?? ''),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            child: Align(
+                              alignment: Alignment.bottomRight,
+                              child: Container(
+                                padding: const EdgeInsets.all(8.0),
+                                color: Colors.black,
+                                child: Text(
+                                  video.duration ?? '',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 8.0),
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 20.0,
+                                backgroundImage: NetworkImage(
+                                  video.channelImage ?? '',
+                                ),
+                              ),
+                              const SizedBox(width: 8.0),
+                              Expanded(
+                                child: Text(
+                                  video.title ?? '',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              const Icon(Icons.more_horiz, color: Colors.grey),
+                            ],
+                          ),
+                          const SizedBox(height: 5.0),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 48.0),
+                            child: Text(
+                              '${video.viewers ?? ''} views · ${videoController.formattedDate(video.dateAndTime)}',
+                              style: const TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                          const Divider(),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
