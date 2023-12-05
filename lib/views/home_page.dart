a1154843821b2d372dd2fde1f416079ab4adb90f
Asif Iqbal <asif.iq.024@gmail.com>
2023-12-06 00:06:44 +0600

    HomePage (With proper styling)

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:video_player_app/controllers/video_controller.dart';
@@ -11,6 +13,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       body: SingleChildScrollView(
         controller: videoController.scrollController,
         padding: const EdgeInsets.all(16.0),
@@ -38,65 +41,87 @@ class HomePage extends StatelessWidget {
                     itemBuilder: (context, index) {
                       final Results video = videoController.videosList[index];
 
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: double.infinity,
-                            height: 200.0,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: NetworkImage(video.thumbnail ?? ''),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            child: Align(
-                              alignment: Alignment.bottomRight,
-                              child: Container(
-                                padding: const EdgeInsets.all(8.0),
-                                color: Colors.black,
-                                child: Text(
-                                  video.duration ?? '',
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 8.0),
-                          Row(
+                      return GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          margin: const EdgeInsets.only(bottom: 20.0),
+                          color: Colors.white,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              CircleAvatar(
-                                radius: 20.0,
-                                backgroundImage: NetworkImage(
-                                  video.channelImage ?? '',
+                              Container(
+                                width: double.infinity,
+                                height: 200.0,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: NetworkImage(video.thumbnail ?? ''),
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(width: 8.0),
-                              Expanded(
-                                child: Text(
-                                  video.title ?? '',
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
+                                child: Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Container(
+                                    padding: const EdgeInsets.all(3.0),
+                                    margin: const EdgeInsets.symmetric(
+                                      horizontal: 10.0,
+                                      vertical: 5.0,
+                                    ),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(3.0),
+                                      color: Colors.black,
+                                    ),
+                                    child: Text(
+                                      video.duration ?? '',
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 12.0,
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
-                              const Icon(Icons.more_horiz, color: Colors.grey),
+                              const SizedBox(height: 8.0),
+                              Row(
+                                children: [
+                                  const SizedBox(width: 15.0),
+                                  CircleAvatar(
+                                    radius: 20.0,
+                                    backgroundImage: NetworkImage(
+                                      video.channelImage ?? '',
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8.0),
+                                  Expanded(
+                                    child: Container(
+                                      margin: const EdgeInsets.all(5.0),
+                                      child: Text(
+                                        video.title ?? '',
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const Icon(Icons.more_vert,
+                                      color: Colors.grey),
+                                  const SizedBox(width: 15.0),
+                                ],
+                              ),
+                              const SizedBox(height: 5.0),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 68.0),
+                                child: Text(
+                                  '${video.viewers ?? ''} views . ${videoController.formattedDate(video.dateAndTime)}',
+                                  style: const TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 10.0),
                             ],
                           ),
-                          const SizedBox(height: 5.0),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 48.0),
-                            child: Text(
-                              '${video.viewers ?? ''} views · ${videoController.formattedDate(video.dateAndTime)}',
-                              style: const TextStyle(
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ),
-                          const Divider(),
-                        ],
+                        ),
                       );
                     },
                   );
